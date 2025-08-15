0f62983a9b4cdf35b53386212026d0d878d8737a
eko777e <replitserver445@gmail.com>
2025-08-15 22:08:36 +0400

    Update start.py

--- InflexMusic/utils/inline/start.py
+++ InflexMusic/utils/inline/start.py
@@ -10,7 +10,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
     ]
     return buttons
@@ -27,9 +27,10 @@ def private_panel(_):
         [   InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-        ],
-        [   InlineKeyboardButton(text=_["S_B_12"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_12"], url=config.SUPPORT_CHANNEL),
+        ]
+        [
+            İnlineKeyboardButton(text=_["S_B_13"], url=config.VOCE_ID),
         ],
     ]
     return buttons
