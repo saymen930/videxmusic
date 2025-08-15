0a4e590cf6fa9fec03e2a8016f39780cf90d6f1e
eko777e <replitserver445@gmail.com>
2025-08-16 00:12:39 +0400

    Update start.py

--- InflexMusic/utils/inline/start.py
+++ InflexMusic/utils/inline/start.py
@@ -1,5 +1,4 @@
 from pyrogram.types import InlineKeyboardButton
-
 import config
 from InflexMusic import app
 
@@ -9,14 +8,13 @@ def start_panel(_):
         [ 
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHANNEL),
             InlineKeyboardButton(text=_["S_B_13"], url=config.SUPPORT_GROUP),
-        ]
+        ],
         [
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
         ]
-    ],
-        
+    ]
     return buttons
 
 
@@ -31,11 +29,11 @@ def private_panel(_):
         [  
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
             InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
-        ]
+        ],
         [
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHANNEL),
             InlineKeyboardButton(text=_["S_B_13"], url=config.SUPPORT_GROUP),
-        ]
+        ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
